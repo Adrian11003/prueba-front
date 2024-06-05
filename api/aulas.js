25592a2a336ce27d89720c94713a5acd85a53f75
Gabriel Ventura Moreno <104277821+gabsss1@users.noreply.github.com>
2024-06-05 23:02:05 +0000

    Add aulas getall and all apis

--- api/aulas.js
+++ api/aulas.js
@@ -14,3 +14,54 @@ export const getAulas = async () => {
         throw error;
     }
 };
+
+export const createAulas = async (aulas) => {
+    try {
+        const response = await api.post('https://api-vn-dola.onrender.com/aulas', aulas)
+
+        return response.data
+    } catch (error) {
+        console.error('Error al crear aulas')
+    }
+}
+
+export const getAulaById = async (aulas_id) => {
+    try {
+        const response = await api.get(`https://api-vn-dola.onrender.com/aulas/${aulas_id}`);
+
+        return response.data;
+    } catch (error) {
+        console.error(`Error al obtener el aula con ID ${aulas_id}`, error);
+        throw error;
+    }
+}
+
+export const updateAlumno = async (aulas_id, updatedAulas) => {
+    try {
+        const response = await api.put(`https://api-vn-dola.onrender.com/aulas/${aulas_id}`, updatedAulas);
+
+        return response.data;
+    } catch (error) {
+        console.error(`Error al actualizar el aula con ID ${aulas_id}`, error);
+        throw error;
+    }
+};
+
+export const deleteAulas = async (aulas_id) => {
+    const aulaId = Number(aulas_id); // Convertir id a número
+
+    console.log("id recibido: ",aulas_id);
+    
+    if (isNaN(aulaId)) {
+        throw new Error('ID inválido');
+    }
+
+    try {
+        const response = await api.delete(`https://api-vn-dola.onrender.com/aulas/${aulas_id}`);
+
+        return response.data;
+    } catch (error) {
+        console.error(`Error al eliminar el aula con ID ${aulas_id}`, error);
+        throw error;
+    }
+};
--- src/views/tables/AulasTable.js
+++ src/views/tables/AulasTable.js
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from 'react';
 import Swal from 'sweetalert2'
-import { getApoderados, deleteApoderado } from "/api/apoderados"
+import { getAulas, deleteAulas } from "api/aulas"
 
 // MUI
 import React from 'react';
@@ -11,17 +11,17 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import EditIcon from '@mui/icons-material/Edit';
 import Link from "next/link"
 
-const ApoderadosTable = () => {
+const AulasTable = () => {
 
-    const [apoderados, setApoderados] = useState([]);
+    const [aulas, setAulas] = useState([]);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const data = await getApoderados();
-                setApoderados(data);
+                const data = await getAulas();
+                setAulas(data);
             } catch (error) {
-                console.error('Error al obtener los datos de los apoderados:', error);
+                console.error('Error al obtener los datos de las aulas:', error);
             }
         };
 
@@ -29,39 +29,43 @@ const ApoderadosTable = () => {
     }, []);
 
     const handleDelete = async (id) => {
-        // Mostrar el SweetAlert2 de confirmación
-        Swal.fire({
-          title: "¿Estás seguro?",
-          text: "¡No podrás revertir esto!",
-          icon: "warning",
-          showCancelButton: true,
-          confirmButtonColor: "#3085d6",
-          cancelButtonColor: "#d33",
-          confirmButtonText: "Sí, eliminarlo",
-        }).then(async (result) => {
-          if (result.isConfirmed) {
-            try {
-              await deleteApoderado(id);
-              const updateApoderado = apoderados.filter((apoderado) => apoderado.id !== id);
-              setApoderados(updateApoderado);
-              Swal.fire({
-                title: "¡Eliminado!",
-                text: "Tu Apoderado ha sido eliminado.",
-                icon: "success",
-              });
-            } catch (error) {
-              console.error("Error al eliminar el apoderado:", error);
-
-              // Mostrar SweetAlert2 de error si ocurre algún problema
-              Swal.fire({
-                title: "Error",
-                text: "Hubo un problema al eliminar el apoderado.",
-                icon: "error",
-              });
-            }
+      // Mostrar el SweetAlert2 de confirmación
+      Swal.fire({
+        title: "¿Estás seguro?",
+        text: "¡No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Sí, eliminarlo",
+      }).then(async (result) => {
+        if (result.isConfirmed) {
+          try {
+            await deleteAulas(id);
+            const updateAula = aulas.filter((aulas) => aulas.id !== id);
+            setAulas(updateAula);
+            Swal.fire({
+              title: "¡Eliminado!",
+              text: "Tu aula ha sido eliminado.",
+              icon: "success",
+            }).then(() => {
+              // Recargar la página después de mostrar la alerta de éxito
+              window.location.reload();
+            });
+          } catch (error) {
+            console.error("Error al eliminar el aula:", error);
+  
+            // Mostrar SweetAlert2 de error si ocurre algún problema
+  
+            Swal.fire({
+              title: "Error",
+              text: "Hubo un problema al eliminar el aula.",
+              icon: "error",
+            });
           }
-        });
-      };
+        }
+      });
+    };
 
   const [searchTerm, setSearchTerm] = useState('');
 
@@ -69,12 +73,10 @@ const ApoderadosTable = () => {
     setSearchTerm(event.target.value);
   };
 
-  const filteredApoderados = apoderados.filter((apoderado) =>
-    apoderado.nombres_apoderado.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    apoderado.apellidos_apoderado.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    apoderado.email_apoderado.toLowerCase().includes(searchTerm.toLowerCase()) ||
-    apoderado.telefono_apoderado.includes(searchTerm) ||
-    String(apoderados.numero_dni).includes(searchTerm)
+  const filteredAulas = aulas.filter((aulas) =>
+    aulas.numero_aula.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    aulas.capacidad.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    aulas.piso.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
   return (
@@ -103,29 +105,30 @@ const ApoderadosTable = () => {
           <TableHead>
             <TableRow>
               <TableCell>Número</TableCell>
+              <TableCell>Capacidad</TableCell>
               <TableCell>Piso</TableCell>
-              <TableCell>Sección</TableCell>
               <TableCell>Acción</TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
-            {filteredApoderados.map((apoderados, index) => (
+            {filteredAulas.map((aulas, index) => (
               <TableRow key={index}>
-                <TableCell>{apoderados.nombres_apoderado}</TableCell>
-                <TableCell>{apoderados.apellidos_apoderado}</TableCell>
-                <TableCell>{apoderados.email_apoderado}</TableCell>
+                <TableCell>{aulas.numero_aula}</TableCell>
+                <TableCell>{aulas.capacidad}</TableCell>
+                <TableCell>{aulas.piso}</TableCell>
 
                 <TableCell>
 
                   <IconButton>
-                    <Link href={`/Apoderados/${apoderados.apoderado_id}` }passHref>
+                    <Link href={`/Aulas/${aulas.aulas_id}` }passHref>
                       <EditIcon />
                     </Link>
                   </IconButton>
 
-                  <IconButton onClick={() => handleDelete(apoderados.apoderado_id)}>
+                  <IconButton onClick={() => handleDelete(aulas.aulas_id)}>
                     <DeleteIcon />
                   </IconButton>
+
                 </TableCell>
               </TableRow>
             ))}
@@ -136,4 +139,4 @@ const ApoderadosTable = () => {
   );
 };
 
-export default ApoderadosTable
+export default AulasTable
